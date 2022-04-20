f331083f2f1f3dad95061abbd3c3095040c1fe3e
SimoneScapin <simonescap87@gmail.com>
2022-04-20 16:11:19 +0200

    snack 5 fatto

--- snack5/index.php
+++ snack5/index.php
@@ -13,16 +13,17 @@
     
     <?php
     $paragraph = "È universalmente riconosciuto che un lettore che osserva il layout di una pagina. viene distratto dal contenuto testuale se questo è leggibile.
-                    Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano 
-                    brevi frasi ripetute, ad esempio “testo qui”). apparendo come un normale blocco di testo leggibile. Molti software di impaginazione
-                    e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni.
-                    a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).";
-
-    $modifiedParagraph =  substr($paragraph, strpos($paragraph, ".") + 1); 
-    var_dump($modifiedParagraph);           
-    ?>
-    
-    <?php ?>
+Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano 
+brevi frasi ripetute, ad esempio “testo qui”). apparendo come un normale blocco di testo leggibile. Molti software di impaginazione
+e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni.
+a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).";?>
+<?php
+    //Suddivido il paragrafo in altri paragrafi in un array
+    $array = explode("." , $paragraph );
+    var_dump($array);             
+    for ( $i = 0 ; $i < count($array) ; $i++){?>
+    <h3> <?php echo $array[$i]; ?> </h3>
+    <?php }?>
     
 </body>
 </html>
